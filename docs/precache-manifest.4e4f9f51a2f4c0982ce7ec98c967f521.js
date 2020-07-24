75c693250ddf0b5b40f91e4ee21d367821ab3273
Julian Marmier <julianmarmier@gmail.com>
2020-07-24 13:55:52 -0400

    Add website to docs

---
+++ docs/precache-manifest.4e4f9f51a2f4c0982ce7ec98c967f521.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b86bba49abb89286fb6894f3761ff3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "052d042fda755ad3b6b1",
+    "url": "/static/css/main.ce4ae7e9.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "052d042fda755ad3b6b1",
+    "url": "/static/js/main.47ba8a1d.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "b7ba55145691c1fd65603ba44b0db22a",
+    "url": "/static/media/logo.b7ba5514.svg"
+  }
+]);
