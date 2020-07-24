78ec8ddb5263361c4264b624c8882faf83d3e428
Julian Marmier <julianmarmier@gmail.com>
2020-07-24 13:15:08 -0400

    Update website

--- forge.config.js
+++ forge.config.js
@@ -1,42 +1,68 @@
+const path = require('path')
+
 module.exports = {
-  makers: [
-    {
-      "name": "@electron-forge/maker-deb",
-      "config": {
-        "options": {
-          "homepage": "https://filemaster.julianm.tk",
-          "icon": "icons/icon.png",
-          "name": "filemaster",
-          "genericName": "filemaster"
+  "packagerConfig": {
+        "icon": "icons/icon.icns"
+      },
+      "makers": [
+        {
+          "name": "@electron-forge/maker-deb",
+          "config": {
+            "options": {
+              "homepage": "https://filemaster.julianm.tk",
+              "icon": "icons/icon.png",
+              "name": "filemaster",
+              "genericName": "filemaster"
+            }
+          }
+        },
+        {
+          "name": "@electron-forge/maker-dmg",
+          "config": {
+            "icon": "icons/icon.icns",
+            "name": "filemaster",
+            "background": "icons/background.png",
+            "contents": [
+              {
+                "x": 400,
+                "y": 200,
+                "type": "link",
+                "path": "/Applications"
+              },
+              {
+                "x": 150,
+                "y": 200,
+                "type": "file",
+                "path": "/Users/julian/Downloads/GitHub/filemaster/out/filemaster-darwin-x64/filemaster.app"
+              }
+            ]
+          }
+        },
+        {
+          "name": "@electron-forge/maker-rpm",
+          "config": {
+            "options": {
+              "homepage": "https://filemaster.julianm.tk",
+              "icon": "icons/icon.png",
+              "name": "filemaster",
+              "genericName": "filemaster"
+            }
+          }
+        },
+        {
+          "name": "@electron-forge/maker-squirrel",
+          "config": {
+            "iconUrl": path.join(__dirname, "icons", "icon.ico"),
+            "setupIcon": path.join(__dirname, "icons", "icon.ico")
+          }
+        },
+        {
+          "name": "@electron-forge/maker-zip",
+          "platforms": [
+            "darwin",
+            "linux",
+            "windows"
+          ]
         }
-      }
-    },
-    {
-      "name": "@electron-forge/maker-dmg",
-      "config": {
-        "icon": "icons/icon.icns",
-        "name": "filemaster",
-        "background": "icons/background.png"
-      }
-    },
-    {
-      "name": "@electron-forge/maker-rpm",
-      "config": {
-        "options": {
-          "homepage": "https://filemaster.julianm.tk",
-          "icon": "icons/icon.png",
-          "name": "filemaster",
-          "genericName": "filemaster"
-        }
-      }
-    },
-    {
-      "name": "@electron-forge/maker-squirrel",
-      "config": {
-        "exe": "filemaster",
-        "iconUrl": "icons/icon.ico",
-        "setupIcon": "icons/icon.ico"
-      }
-    }
-  ]
+      ]
 }
--- website/src/App.js
+++ website/src/App.js
@@ -2,6 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getPlatform() {
+  const osString = navigator.appVersion
+  let os = null, link = "https://github.com/julianmarmier/filemaster/releases/latest"
+  if (osString.includes("Win")) {os = "Windows"; link = "https://github.com/julianmarmier/filemaster/releases/latest/download/filemaster.setup.exe"}
+  else if (osString.includes("Mac")) {os = "macOS"; link = "https://github.com/julianmarmier/filemaster/releases/latest/download/filemaster.dmg"}
+  else if (osString.includes("Linux")) {os = "Linux"; link = {"deb": "https://github.com/julianmarmier/filemaster/releases/latest/download/filemaster.deb", "rpm": "https://github.com/julianmarmier/filemaster/releases/latest/download/filemaster.rpm"}}
+
+  return {"operatingSystem": os, "link": link}
+}
+
+function DownloadLink() {
+  const osInfo = getPlatform()
+  if (osInfo.operatingSystem === "Linux") {
+    // two links
+    return (
+      <>
+        <a className="App-link" href={osInfo.link.deb}>
+          Download deb package
+        </a>
+        <a className="App-link" href={osInfo.link.rpm}> 
+          Download rpm package
+        </a>
+      </>
+    )
+  } else if (osInfo.operatingSystem === null) {
+    // not available for your platform
+    return (
+      <a className="App-link unavailable">Unavailable for your platform</a>
+    )
+  } else {
+    return (
+      <a 
+        className="App-link"
+        href={osInfo.link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Download for {osInfo.operatingSystem}  
+      </a>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +52,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>filemaster</h1>
         <h2>The missing file manager for your computer.</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Download now.
-        </a>
+        <DownloadLink />
+        <p>{getPlatform() ? `Not downloading for ${getPlatform().operatingSystem}? ` : null }<a href="https://github.com/julianmarmier/filemaster/releases/latest" >See all releases.</a></p>
       </header>
       <img className="screenshot" src="filemaster-screenshot.png" alt="screenshot of app"/>
     </div>
